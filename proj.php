507292b2df77a86257b9318f4148de3f096fd794
Srikant V <sv092003@gmail.com>
2020-11-04 00:50:58 -0500

    fixed up icons

--- proj.php
+++ proj.php
@@ -121,6 +121,7 @@
             <?php        
                 }
             ?>
+
             <footer class="footer bg-black small text-center text-white-50"><div class="container">Srikant Vasudevan 2020</div></footer>
         </center>
 
