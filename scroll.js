6b0a77709ed2a3a6e3b1e0ecbe165f255dc8afda
Srikant V <sv092003@gmail.com>
2021-03-09 15:40:26 -0500

    continue refactoring

--- scroll.js
+++ scroll.js
@@ -1,16 +1,32 @@
+//scroll for taskit
+
 const html = document.documentElement;
-const canvas = document.getElementById("resume-canvas");
+const canvas = document.getElementById("hydantoin");
 const context = canvas.getContext("2d");
+
+const hydantoinCanvas = document.getElementById("taskit");
+const hydantoinContext = canvas.getContext("2d");
 
 const frameCount = 100;
 const currentFrame = index => (
   `http://www.srikantv.com/assets/HydantoinFrames/hydantoinFrames${index.toString().padStart(3, '0')}.jpg`
 )
 
+const currentFrame1 = index => (
+  `http://www.srikantv.com/assets/TaskitFrames/taskitFrames${index.toString().padStart(3, '0')}.jpg`
+)
+
 const preloadImages = () => {
   for (let i = 1; i < frameCount; i++) {
     const img = new Image();
     img.src = currentFrame(i);
+  }
+};
+
+const preloadImages1 = () => {
+  for (let i = 1; i < frameCount; i++) {
+    const img = new Image();
+    img.src = currentFrame1(i);
   }
 };
 
@@ -22,9 +38,22 @@ img.onload=function(){
   context.drawImage(img, 0, 0);
 }
 
+const himg = new Image()
+himg.src = currentFrame1(1);
+hydantoinCanvas.width=1500;
+hydantoinCanvas.height=1154;
+himg.onload=function(){
+  hydantoinContext.drawImage(img, 0, 0);
+}
+
 const updateImage = index => {
   img.src = currentFrame(index);
   context.drawImage(img, 0, 0);
+}
+
+const updateImage1 = index => {
+  himg.src = currentFrame1(index);
+  hydantoinContext.drawImage(himg, 0, 0);
 }
 
 window.addEventListener('scroll', () => {  
@@ -35,8 +64,10 @@ window.addEventListener('scroll', () => {
     frameCount - 1,
     Math.ceil(scrollFraction * frameCount)
   );
-  console.log(scrollFraction);
+  requestAnimationFrame(() => updateImage1(frameIndex + 1))
   requestAnimationFrame(() => updateImage(frameIndex + 1))
 });
 
 preloadImages();
+preloadImages1();
+
