ea0ee72c7b61e6756091896ad63757bbd5072898
Srikant V <sv092003@gmail.com>
2021-04-15 20:28:20 -0400

    basic home anim

---
+++ templates/head.php
@@ -0,0 +1,21 @@
+<head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title><?php echo $title?></title>
+        <link rel="icon" type="image/x-icon" href="icons/favicon.png" />
+
+        <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+
+        <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+
+        <link href="css/styles.css" rel="stylesheet" />
+        <link
+    	rel="stylesheet"
+    	href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"
+  		/>
+		<script src="js/wow.min.js"></script>
+        
+    </head>
